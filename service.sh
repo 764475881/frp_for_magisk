da9c1ba3fe24c7c07e70de2222fcbdef29e8c7b0
伤月s <764475881@qq.com>
2025-01-02 19:34:26 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,5 +1,6 @@
 #!/data/adb/magisk/busybox sh
 MODDIR=${0%/*}
+# MODDIR="$(dirname $(readlink -f "$0"))"
 echo $MODDIR >> ${MODDIR}/frpc.log
 
 chmod 755 ${MODDIR}/*
@@ -8,15 +9,15 @@ CONF=$(cat ${MODDIR}/frpc.toml)
 while true; do
     CONF2=$(cat ${MODDIR}/frpc.toml)
     #如果配置文件与之前的不一致，则停止任务等5秒后重启
-    if $CONF2!=$CONF; then
+    if [ "$CONF2" != "$CONF" ]; then
         echo "检测到配置修改，即将停止任务待重启" >> ${MODDIR}/frpc.log
         PID=$(ps -ef|grep "${MODDIR}/frpc -c ${MODDIR}/frpc.toml" | awk '{print $2}')
-        kill PID
-        $CONF=$CONF2
-        $CONF=''
+        kill $PID
+        CONF=$CONF2
+        CONF2=''
         sleep 5
     fi
-    if ! pgrep -f "${MODDIR}/frpc" > ${MODDIR}/frpc.log; then
+    if ! pgrep -f "${MODDIR}/frpc" >> ${MODDIR}/frpc.log; then
         echo "检测任务已停止，开始启动" >> ${MODDIR}/frpc.log
         nohup ${MODDIR}/frpc -c ${MODDIR}/frpc.toml >> ${MODDIR}/frpc.log 2>&1 &
         echo "启动完成" >> ${MODDIR}/frpc.log
